5ec4317c55379b861d25ed2e6b35cb811cc2dcb8
SrAxi <riccardo_polacci@hotmail.com>
2018-06-27 16:09:22 +0200

    Setting up socket.io. Setting up connect/disconnect listeners in client & server. Broadcasting a events client/server.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,35 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-let app = express();
 const PORT = process.env.PORT || 3000;
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+
+server.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
 });
 
